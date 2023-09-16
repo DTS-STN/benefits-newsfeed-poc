e6127ad0ba3f0e0f2743d0dccf490143139e8a2d
frank basham <frankabasham@gmail.com>
2023-09-16 14:47:42 -0400

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,162 @@
-import Image from 'next/image'
+import { IoSearch } from "react-icons/io5";
+import { FaChevronDown } from "react-icons/fa";
+import { BsChevronRight } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className="mb-2">
+        <div className="border-b-primary border-b">
+          <div className="flex flex-col md:flex-row md:items-center justify-between gap-10 px-4 md:px-20 py-10">
+            <div className="flex justify-between md:gap-10">
+              <img src="/goc.svg" className="h-6 md:h-8" />
+              <a className="block md:hidden">FR</a>
+            </div>
+            <div className="flex flex-col">
+              <a href="#" className="underline mb-2 self-end hidden md:block">
+                Français
+              </a>
+              <div className="flex">
+                <label htmlFor="search-canada-ca" className="sr-only">
+                  Search Canada.ca
+                </label>
+                <input
+                  id="search-canada-ca"
+                  placeholder="Search Canada.ca"
+                  className="border px-3 py-1.5 outline-none"
+                ></input>
+                <button className="bg-primary self-stretch px-3 py-1.5 text-[20px] text-white">
+                  <IoSearch />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <div className="px-4 md:px-20">
+          <button className="bg-primary flex items-center gap-2 px-5 py-2.5 text-white">
+            Menu
+            <span>
+              <FaChevronDown />
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </button>
+          <div className="flex items-center gap-2 underline mt-5">
+            <a>Canada.ca</a>
+            <BsChevronRight />
+            <a>Benefit News</a>
+          </div>
+        </div>
+      </header>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <main className="px-4 md:px-20 py-10 md:py-20 bg-gray-100 flex flex-col md:flex-row gap-10">
+        <section className="md:mt-32">
+          <h3 className="font-semibold text-gray-500">Filter News by Benefit</h3>
+          <div className="flex flex-col items-start">
+            <label className="flex gap-2">
+              <input type="checkbox" name="" id="" />
+              filter
+            </label>
+            <label className="flex gap-2">
+              <input type="checkbox" name="" id="" />
+              filter
+            </label>
+            <label className="flex gap-2">
+              <input type="checkbox" name="" id="" />
+              filter
+            </label>
+            <label className="flex gap-2">
+              <input type="checkbox" name="" id="" />
+              filter
+            </label>
+            <label className="flex gap-2">
+              <input type="checkbox" name="" id="" />
+              filter
+            </label>
+            <label className="flex gap-2">
+              <input type="checkbox" name="" id="" />
+              filter
+            </label>
+          </div>
+        </section>
+        <section className="grow">
+          <h1 className="text-4xl font-semibold mb-8">Benefit News Updates</h1>
+          <div className="flex mb-5">
+            <label htmlFor="search-benefit" className="sr-only">
+              Search by benefit name or source
+            </label>
+            <input
+              id="search-benefit"
+              placeholder="Search by benefit name or source"
+              className="border px-3 py-1.5 outline-none w-full"
+            ></input>
+            <button className="bg-primary self-stretch px-3 py-1.5 text-[20px] text-white">
+              <IoSearch />
+            </button>
+          </div>
+          <div className="bg-white px-2 md:px-10 py-5 divide-y space-y-4">
+            <div className="py-4">
+              <div className="flex flex-col md:flex-row gap-2">
+                <span className="text-gray-500 text-sm">Guaranteed Income Supplement | September 15, 2023</span>
+                <span className="bg-green-500 text-white px-2 py-0.5 font-semibold text-sm">Update</span>
+              </div>
+              <h2 className="text-2xl font-semibold">One time grant</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Adipisci, qui?
+              </p>
+            </div>
+            <div className="py-4">
+              <div className="flex flex-col md:flex-row gap-2">
+                <span className="text-gray-500 text-sm">Guaranteed Income Supplement | September 15, 2023</span>
+                <span className="bg-green-500 text-white px-2 py-0.5 font-semibold text-sm">Update</span>
+              </div>
+              <h2 className="text-2xl font-semibold">One time grant</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Adipisci, qui?
+              </p>
+            </div>
+            <div className="py-4">
+              <div className="flex flex-col md:flex-row gap-2">
+                <span className="text-gray-500 text-sm">Guaranteed Income Supplement | September 15, 2023</span>
+                <span className="bg-orange-500 text-white px-2 py-0.5 font-semibold text-sm">New</span>
+              </div>
+              <h2 className="text-2xl font-semibold">One time grant</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Adipisci, qui?
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer>
+        <div className="bg-primary text-white bg-[url('/footer_bg_img.svg')] bg-right-bottom bg-no-repeat min-h-[200px] px-4 md:px-20 py-10">
+          <h3 className="font-bold text-lg mb-4">Government of Canada</h3>
+          <div className="grid md:grid-cols-3 gap-2 md:gap-4 md:place-content-center">
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+          </div>
+        </div>
+        <div className="flex justify-between px-4 md:px-20 py-10">
+          <div className="flex flex-col md:flex-row gap-2">
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+            <a>link</a>
+          </div>
+          <div className="flex items-end">
+            <img src="/wmms-blk.svg" className="h-8"></img>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
