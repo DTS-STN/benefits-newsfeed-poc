dd17f60d89e4ce23210c746bffe4c301c01066d0
frank basham <frankabasham@gmail.com>
2023-09-20 14:36:12 -0400

    add very crude pagination

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,6 +2,7 @@ import { IoSearch } from "react-icons/io5";
 import Card, { NewsItem } from "@/components/Card";
 import Filter from "@/components/Filter";
 import { useTranslation } from "@/app/i18n";
+import Paginate from "@/components/Paginate";
 
 type PageProps = {
   params: {
@@ -15,7 +16,7 @@ export default async function Home({ params }: PageProps) {
   const { t } = await useTranslation(lang, "common");
 
   const res = await fetch("http://localhost:3000/api");
-  const { data } = await res.json();
+  const data = await res.json();
 
   const programs: Set<string> = new Set(
     data.map(({ program }: NewsItem) => program)
@@ -24,7 +25,7 @@ export default async function Home({ params }: PageProps) {
   return (
     <main className="py-10 md:py-20 bg-gray-100">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-10 px-4">
-        <Filter lang={lang} programs={programs}/>
+        <Filter lang={lang} programs={programs} />
         <section className="grow">
           <h1 className="text-4xl font-semibold mb-8">{t("main.h1")}</h1>
           <div className="flex mb-5">
@@ -40,11 +41,7 @@ export default async function Home({ params }: PageProps) {
               <IoSearch />
             </button>
           </div>
-          <div className="bg-white px-2 md:px-10 py-5 divide-y space-y-4">
-            {data.map((item: NewsItem) => (
-              <Card {...item} key={item.id} />
-            ))}
-          </div>
+          <Paginate data={data} />
         </section>
       </div>
     </main>
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,5 +5,5 @@ import data from "@/lib/data.json";
 // Fake data request
 
 export async function GET(request: Request) {
-  return NextResponse.json({ data });
+  return NextResponse.json(data);
 }
