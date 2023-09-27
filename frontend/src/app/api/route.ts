e43bb5a11b3308b919df2d838d4a75db49f3e24f
frank basham <frankabasham@gmail.com>
2023-09-27 12:45:44 -0400

    scaffold out fetching from django rest api

--- frontend/src/app/api/route.ts
+++ frontend/src/app/api/route.ts
@@ -5,5 +5,11 @@ import data from "@/lib/data.json";
 // Fake data request
 
 export async function GET(request: Request) {
+  
+  // fetch from the Django rest api
+  const res = await fetch('http://localhost:8000')
+  const resData = await res.json()
+
+  // TODO: swap data with resData after seeding more data in the admin ui
   return NextResponse.json(data);
 }
