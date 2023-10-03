c43295f3689d04c12a0a0264830a68272a3c26e6
frank basham <frankabasham@gmail.com>
2023-10-03 09:52:27 -0400

    add token authentication

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,14 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from adminui.models import NewsItem
 from .serializers import NewsItemSerializer
 
 
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def getData(request):
     news_items = NewsItem.objects.all()
     serializer = NewsItemSerializer(news_items, many=True)
