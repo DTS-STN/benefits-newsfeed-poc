d6824339bb1df4a2591c0d3602b21df513e096eb
frank basham <frankabasham@gmail.com>
2023-10-04 11:39:39 -0400

    add seed fixtures and additional api endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData)
+    path('api/v1/get-all-news-items', views.get_data),
+    path('api/v1/get-news-items-by/<search>', views.get_data_by_search),
 ]
